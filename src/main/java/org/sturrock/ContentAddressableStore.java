8ac85e198f4c243e96649174a77cdf945bfe1939
andysturrock <github@sturrock.org>
2015-04-25 16:45:47 +0100

    Initial naive implementation.  No tests.  getHashes() not implemented yet

---
+++ src/main/java/org/sturrock/ContentAddressableStore.java
@@ -0,0 +1,78 @@
+package org.sturrock;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/*
+ * Copyright 2015 Andy Sturrock
+ * Derived from https://github.com/drewnoakes/cassette
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Defines a content-addressable store
+ */
+public interface ContentAddressableStore {
+	/**
+	 * Write content to the store, returning its hash.
+	 * If the store already contains this content, the write is discarded but
+	 * the hash is returned as normal.
+	 * @param stream	Content to be written
+	 * @return			hash of content
+	 * @throws IOException
+	 */
+	byte[] write(InputStream stream) throws IOException;
+	
+	/**
+	 * Check whether content exists in the store with the specified hash
+	 * @param hash	Hash of content to check
+	 * @return		<code>true</code> if content exists in the store with
+	 * 				specified hash
+	 */
+	boolean contains(byte[] hash);
+
+	/**
+	 * Read content from the store.
+	 * @param hash	The hash of the content to read.
+	 * @return		<code>InputStream</code> of content .
+	 * @throws FileNotFoundException
+	 */
+	InputStream read(byte[] hash) throws FileNotFoundException;
+	
+	/**
+	 * Get the length of the content with the specified hash
+	 * @param hash	The hash of the content
+	 * @return		The length of the content, or -1 if no content with the specified hash exists.
+	 * @throws IOException
+	 */
+	long getContentLength(byte[] hash) throws IOException;
+
+	/**
+	 * Get a list of all hashes in the store.  The list is generated lazily
+	 * by querying the filesystem, and thus will not behave deterministically if
+	 * more content is added while this function is called.
+	 * @return	List of hashes in the store.
+	 */
+	List<byte[]> getHashes();
+
+	/**
+	 * Delete content from the store.
+	 * @param	hash Hash of content to delete
+	 * @return	<code>true</code> if the content existed and was deleted; otherwise false.
+	 * @throws IOException
+	 */
+	boolean delete(byte[] hash) throws IOException;
+}
---
+++ src/main/java/org/sturrock/ContentAddressableStoreImpl.java
@@ -0,0 +1,192 @@
+package org.sturrock;
+
+/*
+ * Copyright 2015 Andy Sturrock
+ * Derived from https://github.com/drewnoakes/cassette
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * A content-addressable store backed by the file system.
+ * Default implementation of ContentAddressableStore.
+ *
+ */
+public final class ContentAddressableStoreImpl implements
+		ContentAddressableStore {
+	/**
+	 * The root path for all content within this store
+	 */
+	private final Path rootPath;
+
+	/**
+	 * The size of the byte array buffer used for read/write operations.
+	 */
+	private final int bufferSize = 4096;
+
+	/**
+	 * The number of characters from the hash to use for the name of
+	 * the top level subdirectories. 
+	 */
+	private final int hashPrefixLength = 4;
+	
+	/**
+	 * Initialises the store, using rootPath as the root for all content.
+	 * @param rootPath	Root path for all content in this store.
+	 * @throws IOException
+	 */
+	public ContentAddressableStoreImpl(String rootPath) throws IOException {
+		if (rootPath == null)
+			throw new IllegalArgumentException("rootPath");
+
+		this.rootPath = Paths.get(rootPath);
+
+		if (!Files.isDirectory(this.rootPath))
+			Files.createDirectory(this.rootPath);
+	}
+
+	public byte[] write(InputStream stream) throws IOException {
+		if (stream == null)
+			throw new IllegalArgumentException("stream");
+
+		MessageDigest messageDigest;
+		try {
+			messageDigest = MessageDigest.getInstance("SHA1");
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalArgumentException(e);
+		}
+		final byte[] buffer = new byte[bufferSize];
+		for (int read = 0; (read = stream.read(buffer)) != -1;) {
+			messageDigest.update(buffer, 0, read);
+		}
+		final byte[] hash = messageDigest.digest();
+		stream.reset();
+
+		String hashString = getHashString(hash);
+
+		// Determine the location for the content file
+		Path contentPath = getContentPath(hashString);
+		Path subPath = getSubPath(hashString);
+
+		// Test whether a file already exists for this hash
+		if (!Files.exists(contentPath)) {
+			// Ensure the sub-path exists
+			if (!Files.isDirectory(subPath))
+				Files.createDirectories(subPath);
+
+			Files.copy(stream, contentPath);
+		}
+
+		// The caller receives the hash, regardless of whether the
+		// file previously existed in the store
+		return hash;
+	}
+
+	public boolean contains(byte[] hash) {
+		if (hash == null)
+			throw new IllegalArgumentException("hash");
+
+		String hashString = getHashString(hash);
+		Path contentPath = getContentPath(hashString);
+
+		return Files.exists(contentPath);
+	}
+
+	public InputStream read(byte[] hash) throws FileNotFoundException {
+		if (hash == null)
+			throw new IllegalArgumentException("hash");
+
+		String hashString = getHashString(hash);
+		Path contentPath = getContentPath(hashString);
+
+		if (!Files.exists(contentPath)) {
+			return null;
+		}
+
+		return new FileInputStream(contentPath.toFile());
+	}
+
+	public long getContentLength(byte[] hash) throws IOException {
+		if (hash == null)
+			throw new IllegalArgumentException("hash");
+
+		String hashString = getHashString(hash);
+		Path contentPath = getContentPath(hashString);
+
+		if (!Files.exists(contentPath)) {
+			return -1;
+		}
+
+		BasicFileAttributes attrs;
+		attrs = Files.readAttributes(contentPath, BasicFileAttributes.class);
+		return attrs.size();
+	}
+
+	public List<byte[]> getHashes()
+	{
+		if(true)
+			throw new RuntimeException("Not implemented yet");
+		
+		return new LinkedList<byte[]>();
+	}
+
+
+	public boolean delete(byte[] hash) throws IOException {
+		String hashString = getHashString(hash);
+		Path contentPath = getContentPath(hashString);
+
+		if (!Files.exists(contentPath))
+			return false;
+
+		Files.delete(contentPath);
+		return true;
+	}
+
+	private Path getContentPath(String hashString) {
+		Path subPath = getSubPath(hashString);
+		Path contentPath = Paths.get(subPath.toString(),
+				hashString.substring(hashPrefixLength));
+		return contentPath;
+	}
+
+	private Path getSubPath(String hashString) {
+		Path subPath = Paths.get(rootPath.toString(),
+				hashString.substring(0, hashPrefixLength));
+		return subPath;
+	}
+
+	private String getHashString(byte[] hash) {
+		Formatter formatter = new Formatter();
+		for (final byte b : hash) {
+			formatter.format("%02x", b);
+		}
+		String hashString = formatter.toString();
+		formatter.close();
+		return hashString;
+	}
+}
