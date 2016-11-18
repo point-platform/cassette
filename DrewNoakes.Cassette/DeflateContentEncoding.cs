9828db96216869b4945e0d08666df945b6608c68
Drew Noakes <git@drewnoakes.com>
2016-11-18 09:54:33 +0000

    Add missing XML documentation.

--- DrewNoakes.Cassette/DeflateContentEncoding.cs
+++ DrewNoakes.Cassette/DeflateContentEncoding.cs
@@ -24,23 +24,29 @@ namespace Cassette
     /// </summary>
     public sealed class DeflateContentEncoding : IContentEncoding
     {
+        /// <inheritdoc />
         public string Name => "deflate";
 
         /// <summary>
-        /// Get and set the compression level to use when writing content.
+        /// Get and set the compression level to use when writing content. Default is <see cref="System.IO.Compression.CompressionLevel.Optimal"/>.
         /// </summary>
         public CompressionLevel CompressionLevel { get; set; }
 
+        /// <summary>
+        /// Initialises a new deflate-based content encoding.
+        /// </summary>
         public DeflateContentEncoding()
         {
             CompressionLevel = CompressionLevel.Optimal;
         }
 
+        /// <inheritdoc />
         public Stream Encode(Stream stream)
         {
             return new DeflateStream(stream, CompressionLevel, leaveOpen: true);
         }
 
+        /// <inheritdoc />
         public Stream Decode(Stream stream)
         {
             return new DeflateStream(stream, CompressionMode.Decompress, leaveOpen: true);
--- DrewNoakes.Cassette/GZipContentEncoding.cs
+++ DrewNoakes.Cassette/GZipContentEncoding.cs
@@ -24,23 +24,29 @@ namespace Cassette
     /// </summary>
     public sealed class GZipContentEncoding : IContentEncoding
     {
+        /// <inheritdoc />
         public string Name => "gzip";
 
         /// <summary>
-        /// Get and set the compression level to use when writing content.
+        /// Get and set the compression level to use when writing content. Default is <see cref="System.IO.Compression.CompressionLevel.Optimal"/>.
         /// </summary>
         public CompressionLevel CompressionLevel { get; set; }
 
+        /// <summary>
+        /// Initialises a new GZip-based content encoding.
+        /// </summary>
         public GZipContentEncoding()
         {
             CompressionLevel = CompressionLevel.Optimal;
         }
 
+        /// <inheritdoc />
         public Stream Encode(Stream stream)
         {
             return new GZipStream(stream, CompressionLevel, leaveOpen: true);
         }
 
+        /// <inheritdoc />
         public Stream Decode(Stream stream)
         {
             return new GZipStream(stream, CompressionMode.Decompress, leaveOpen: true);
